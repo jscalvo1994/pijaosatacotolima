f3d4d4d53089e09b732e23ce9d61bf1f9a66e816
jscalvo1994 <juansebastian.calvo408@comunidadunir.net>
2024-04-30 00:07:32 -0500

    Caracterizacion pt1

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -5,8 +5,7 @@ export default function Home() {
   return (
     <>
       <CarrouselGeneral />
-      <GobernadoresPage />
-     
+      <GobernadoresPage /> 
       <UbicacionPage />
     </>
   );
